b63635c4eeb2212a4a9acad25550174facebfd30
kurushetra <kurushetra@aim.com>
2012-09-26 20:57:18 +0200

    Initial Commit

---
+++ RagaWriterMaster/RWViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWViewController.h
+//  RagaWriterMaster
+//
+//  Created by luis sintes on 26/09/12.
+//  Copyright (c) 2012 luis sintes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWViewController : UIViewController
+
+@end
---
+++ RagaWriterMaster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RagaWriterMaster
+//
+//  Created by luis sintes on 26/09/12.
+//  Copyright (c) 2012 luis sintes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+      @autoreleasepool {
+          return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWAppDelegate class]));
+      }
+}

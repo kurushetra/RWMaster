3925b6c553758c84c4b6afa4db043f299c97f07d
kurushetra <kurushetra@aim.com>
2012-09-30 01:14:45 +0200

    un poco laido ponendo un cell para el editNote Panel

---
+++ RagaWriterMaster/RWNoteEditionView.h
@@ -0,0 +1,13 @@
+//
+//  RWNoteEditionView.h
+//  RagaWriterMaster
+//
+//  Created by luis sintes on 30/09/12.
+//  Copyright (c) 2012 luis sintes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWNoteEditionView : UICollectionViewCell
+
+@end
---
+++ RagaWriterMaster/RWNoteEditionView.m
@@ -0,0 +1,31 @@
+//
+//  RWNoteEditionView.m
+//  RagaWriterMaster
+//
+//  Created by luis sintes on 30/09/12.
+//  Copyright (c) 2012 luis sintes. All rights reserved.
+//
+
+#import "RWNoteEditionView.h"
+
+@implementation RWNoteEditionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
